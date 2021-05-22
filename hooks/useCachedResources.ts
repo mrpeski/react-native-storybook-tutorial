ddbb0506c03bd7b8064487e71c819421695025de
Olayinka Ola <mimoyinka@gmail.com>
2021-05-22 14:50:57 +0100

    Get Started

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -16,6 +16,10 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          percolate: require('../assets/icon/percolate.ttf'),
+          'NunitoSans-Bold': require('../assets/font/NunitoSans-Bold.ttf'),
+          'NunitoSans-Italic': require('../assets/font/NunitoSans-Italic.ttf'),
+          NunitoSans: require('../assets/font/NunitoSans-Regular.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
